195aabea127a045c482dcf1701fa46fea2262467
Saifur Rahman <sr018695@gmail.com>
2023-12-19 16:02:07 +0600

    2nd version came here

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", function(){
   const EnButton = document.querySelector(".css-input");
   
 
-  
+  EnButton.addEventListener("keyup", function(e){
+    const color = e.target.value;
+    if(color){
+      EnButton.value = color.toUpperCase();
+      if(isValidHex(color)) document.body.style.backgroundColor = `#${color}`;
+    }
+  });
 
   chButton.addEventListener("click", showRGB_HEX);
   EnterButton.addEventListener("click", desireBackColor);
@@ -13,6 +19,10 @@ document.addEventListener("DOMContentLoaded", function(){
   });
 });
 
+function isValidHex(color){
+  if (color.length !== 6) return false;
+	return /^[0-9A-Fa-f]{6}$/i.test(color);
+}
 
 
 function desireBackColor(){
@@ -30,18 +40,24 @@ function showRGB_HEX(){
   const g = Math.floor(Math.random()*255);
   const b = Math.floor(Math.random()*255);
 
-  const hr = singleCharHandle(r.toString(16).toUpperCase());
-  const hg = singleCharHandle(g.toString(16).toUpperCase());
-  const hb = singleCharHandle(b.toString(16).toUpperCase());
+  const hr = singleCharHandle(r.toString(16));
+  const hg = singleCharHandle(g.toString(16));
+  const hb = singleCharHandle(b.toString(16));
 
-  const rgb = `rgb(${r},${g},${b})`;
-  const hex = `#${hr}${hg}${hb}`;
+  const rgb = (`rgb(${r},${g},${b})`).toUpperCase();
+  const hex = (`#${hr}${hg}${hb}`).toUpperCase();
   
   document.body.style.backgroundColor = rgb;
 
   document.querySelector(".css-rgb-hex-container").innerHTML = `
-  <button class="css-rgb">${rgb}</button>
-  <button class="css-hex">${hex}</button>
+  <div class="css-rgb-container">
+    <div class="css-title">RGB</div>
+    <button class="css-rgb">${rgb}</button>
+  </div>
+  <div class="css-hex-container">
+    <div class="css-title">HEX</div>
+    <button class="css-hex">${hex}</button>
+  </div>
   `;
 
   const rgbCopyButton = document.querySelector(".css-rgb");
